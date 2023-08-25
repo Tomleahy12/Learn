d8234e734cdf8428ffd566ad29817b4a015a67f3
Tomleahy12 <Tomjleahy12@gmail.com>
2023-08-24 23:36:34 -0400

    condensed reduction algorithm

--- LinAlg.h
+++ LinAlg.h
@@ -15,9 +15,13 @@ namespace LinAlg{
         D2array<T> perm;
     };
     template<class T>
+    D2array<T> reduce(D2array<T> &d2array, bool ones = false, bool modify = false);
+    template<class T>
     PLU<T> PLU_decomp(const D2array<T> &d2array);
     template<class T>
-    D2array<T> REF(const D2array<T> &d2array);
+    D2array<T> partial_pivot(D2array<T> d2array);
+    template<class T>
+    D2array<T> REF(D2array<T> d2array);
     template<class T>
     D2array<T> RREF(D2array<T> d2array);
 }
