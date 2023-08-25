32ad9c0ed05a20df0aa96f97d5f2ae42466ac741
Tomleahy12 <Tomjleahy12@gmail.com>
2023-08-24 22:08:55 -0400

    main

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,9 @@
 int main(){
     float A[]={1,3,5,9,1,3,1,7,4,3,9,7,5,2,0,9};
     D2array<float> Mat_A(4,4,A); 
-    LinAlg::PLU<float> Mat_PLU = LinAlg::reduce(Mat_A);
+    LinAlg::PLU<float> Mat_PLU = LinAlg::PLU_decomp(Mat_A);
     (Mat_PLU.lower).print();
     (Mat_PLU.upper).print();
+    (Mat_PLU.perm).print();
+    (Mat_PLU.upper*Mat_PLU.lower).print();
 }
